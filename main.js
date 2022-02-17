9dc7a61a9a08060fe1a6c3c6a099ec2b439d4633
Michael <miwanek11199@gmail.com>
2022-02-17 13:13:50 -0800

    changed way API works to asynchronous function with await keyword instead of plain promises

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
+
 const img = document.getElementById('catgif_1');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=Y4I1yn9htIQM27xALedrV4xYazIepPqE&s=grumpy cat', {mode: 'cors'})
 
-.then(function(response) {
-    return response.json();
-})
+//this is how to use an API with an asynchronous function, which allows us to use the 'await' keyword
+//async/await are just promises written in a different way
 
-.then(function(response) {
-    console.log(response);
-    console.log(response.data.images.original.url);
-    img.src = response.data.images.original.url;
-})
+async function getCats() {
+    
+    try {
+        fetch('https://api.giphy.com/v1/gifs/translate?api_key=Y4I1yn9htIQM27xALedrV4xYazIepPqE&s=grumpy cat', {mode: 'cors'})
+        const catData = await response.json();
+        console.log(catData);
+        img.src = catData.data.images.original.url;
+    }
 
-.catch(function(err) {
-    // Error :(
-  });
+    catch (err)  {
+        console.log(err);
+    }
+}
+
+//then call asynchronous funciton with this.
+getCats();
+
+
+
+
+//this is the first way shown to use the API with promises
+
+// fetch('https://api.giphy.com/v1/gifs/translate?api_key=Y4I1yn9htIQM27xALedrV4xYazIepPqE&s=grumpy cat', {mode: 'cors'})
+
+// .then(function(response) {
+//     return response.json();
+// })
+
+// .then(function(response) {
+//     console.log(response);
+//     console.log(response.data.images.original.url);
+//     img.src = response.data.images.original.url;
+// })
+
+// .catch(function(err) {
+//     // Error :(
+//   });
